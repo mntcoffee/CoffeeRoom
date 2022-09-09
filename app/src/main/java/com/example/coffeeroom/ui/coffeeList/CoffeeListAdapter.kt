9723df692c61ffda591c7674b18eb649b90272a6
stakasaki <stakasaki707@gmail.com>
2022-09-09 15:16:38 +0900

    create coffeeListAdapter

---
+++ app/src/main/java/com/example/coffeeroom/ui/coffeeList/CoffeeListAdapter.kt
@@ -0,0 +1,59 @@
+package com.example.coffeeroom.ui.coffeeList
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.coffeeroom.R
+import com.example.coffeeroom.data.model.coffee.Coffee
+
+class CoffeeListAdapter : ListAdapter<Coffee, CoffeeListAdapter.CoffeeListViewHolder>(
+    DIFF_CALLBACK
+) {
+
+    // add clickListener
+    private lateinit var listener: OnItemClickListener
+
+    interface OnItemClickListener {
+        fun onClick(coffee: Coffee)
+    }
+
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        this.listener = listener
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoffeeListViewHolder =
+        CoffeeListViewHolder(parent)
+
+    override fun onBindViewHolder(holder: CoffeeListViewHolder, position: Int) {
+        holder.textView.text = "${getItem(position).country} ${getItem(position).farm}"
+        holder.layout.setOnClickListener {
+            listener.onClick(coffee = getItem(position))
+        }
+    }
+
+    class CoffeeListViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView
+        val layout: CardView
+
+        init {
+            textView = view.findViewById(R.id.textview_coffee_item)
+            layout = view.findViewById(R.id.cardview_coffee)
+        }
+    }
+}
+
+private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Coffee>() {
+
+    override fun areItemsTheSame(oldItem: Coffee, newItem: Coffee): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Coffee, newItem: Coffee): Boolean {
+        return oldItem == newItem
+    }
+
+}
