5a6a0fa427fc1424d84058b84e065b0b6810a649
stakasaki <stakasaki707@gmail.com>
2022-09-10 15:00:34 +0900

    implement coffeeDetailFragment

--- app/src/main/java/com/example/coffeeroom/ui/coffeeDetail/CoffeeDetailFragment.kt
+++ app/src/main/java/com/example/coffeeroom/ui/coffeeDetail/CoffeeDetailFragment.kt
@@ -1,20 +1,68 @@
 package com.example.coffeeroom.ui.coffeeDetail
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.coffeeroom.R
+import com.example.coffeeroom.databinding.FragmentCoffeeDetailBinding
+import com.example.coffeeroom.databinding.FragmentCoffeeDetailEditBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class CoffeeDetailFragment : Fragment() {
+
+    private var _binding: FragmentCoffeeDetailBinding? = null
+    private val binding get() = _binding!!
+
+    private val coffeeDetailViewModel: CoffeeDetailViewModel by viewModels()
+
+    private val args: CoffeeDetailFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_coffee_detail, container, false)
+        _binding = FragmentCoffeeDetailBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // coffee ID
+        val coffeeID = args.coffeeID
+        coffeeDetailViewModel.onStart(coffeeID)
+
+        // set text from coffee[coffeeID]
+        coffeeDetailViewModel.coffeeDetail.observe(viewLifecycleOwner) { coffeeDetail ->
+            binding.apply {
+                textviewTitle.text = coffeeDetail.title
+                textviewCountryData.text = coffeeDetail.country
+                textviewFarmData.text = coffeeDetail.farm
+                textviewProcessData.text = coffeeDetail.process
+                textviewRoasterData.text = coffeeDetail.roaster
+                textviewRoastingDegreeData.text = coffeeDetail.roastingDegree
+                textviewComment.text = coffeeDetail.comment
+            }
+        }
+
+        // edit button
+        binding.buttonEdit.setOnClickListener {
+            val action = CoffeeDetailFragmentDirections
+                .actionCoffeeDetailFragmentToCoffeeDetailEditFragment(coffeeID)
+            findNavController().navigate(action)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
