59ffb99de63e409becb53acfb659ed0f56299e3b
stakasaki <stakasaki707@gmail.com>
2022-09-12 02:12:05 +0900

    implemented searchBar in searchFragment

--- app/src/main/java/com/example/coffeeroom/MainActivity.kt
+++ app/src/main/java/com/example/coffeeroom/MainActivity.kt
@@ -2,12 +2,20 @@ package com.example.coffeeroom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import com.example.coffeeroom.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
+
 }
--- app/src/main/java/com/example/coffeeroom/ui/coffeeList/SearchResultFragment.kt
+++ app/src/main/java/com/example/coffeeroom/ui/coffeeList/SearchResultFragment.kt
@@ -1,19 +1,68 @@
 package com.example.coffeeroom.ui.coffeeList
 
+import android.content.Context
+import android.content.Context.INPUT_METHOD_SERVICE
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.navigation.fragment.findNavController
 import com.example.coffeeroom.R
+import com.example.coffeeroom.databinding.FragmentCoffeeListBinding
+import com.example.coffeeroom.databinding.FragmentSearchResultBinding
 
 class SearchResultFragment : Fragment() {
+
+    private var _binding: FragmentSearchResultBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_result, container, false)
+        _binding = FragmentSearchResultBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        showSoftKeyboard(binding.edittextSearch)
+        binding.recyclerViewSearchResult.setOnFocusChangeListener { _, HasFocus ->
+            if(HasFocus) showOffKeyboard()
+        }
+        binding.recyclerViewSearchResult.setOnTouchListener { view, motionEvent ->
+            Log.d("test", "touch")
+            binding.recyclerViewSearchResult.requestFocus()
+            true
+        }
+
+        binding.imageviewBackButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun showSoftKeyboard(view: View) {
+        if (view.requestFocus()) {
+            val imm = requireContext().getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
+        }
+    }
+
+    private fun showOffKeyboard() {
+        val imm = requireContext().getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(binding.root.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
     }
 
 }
