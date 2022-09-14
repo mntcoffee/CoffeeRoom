8d7a9a351b672ff54c460f16c8f37d8383fb8c9f
mntcoffee <stakasaki707@gmail.com>
2022-09-15 00:05:27 +0900

    modify textview_update

--- app/src/main/java/com/example/coffeeroom/ui/coffeeDetail/CoffeeDetailFragment.kt
+++ app/src/main/java/com/example/coffeeroom/ui/coffeeDetail/CoffeeDetailFragment.kt
@@ -1,6 +1,7 @@
 package com.example.coffeeroom.ui.coffeeDetail
 
 import android.os.Bundle
+import android.provider.SyncStateContract.Helpers.update
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -50,7 +51,7 @@ class CoffeeDetailFragment : Fragment() {
                 textviewRoasterData.text = coffeeDetail.roaster
                 textviewRoastingDegreeData.text = coffeeDetail.roastingDegree
                 textviewComment.text = coffeeDetail.comment
-                textviewUpdate.text = coffeeDetail.updatedAt
+                textviewUpdate.text = getString(R.string.updated_at, coffeeDetail.updatedAt)
             }
         }
 
